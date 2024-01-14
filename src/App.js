36f5668683ec977a3490d3a05382f27e948f5f7a
Getnet Alemye <debreroha12@gmail.com>
2024-01-14 19:29:06 +0300

    first routing

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import './App.css';
 import Header from './Header'
 import Home from './Home'
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 
 
 function App() {
   return (
-    <div className="app">
-      <Header />
-      <Home />
-    </div>
+    <Router>
+      <div className="app">
+        <Routes>
+          <Route path="/" element={[<Header key="header" />, <Home key="home" />]} />
+          {/* <Route path='/' element=<Route path="/" element={[<Header key="header" />, <Home key="home" />]} />
+          <Route path='/' element={<Home />} /> */}
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
