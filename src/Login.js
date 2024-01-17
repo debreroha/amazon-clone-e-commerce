bb9737a9fcc6c4d71f784dc8dc1875563756c5f8
Getnet Alemye <debreroha12@gmail.com>
2024-01-17 14:11:56 +0300

    completion of sign in component and starting use of firebase

--- src/Login.js
+++ src/Login.js
@@ -1,19 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Login.css'
 import {Link} from 'react-router-dom'
  
 function Login() {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState()
+
+  const signIn = (e) => {
+    e.preventDefault()
+
+    // do some firabase to signin
+  }
+
+  const register = e => {
+    e.preventDefault()
+
+    // do some firebase to register
+  } 
+
   return (
-    <div Login>
+    <div className='login'>
       <Link to='/'>
-        <img src="https://www.pngimg.com/uploads/amazon/amazon_PNG11.png" alt="amazon logo" className="login__logo" />
+        <img 
+          src='https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/1024px-Amazon_logo.svg.png' 
+          alt="amazon logo" className="login__logo" />
       </Link>
       <div className="login__container">
         <h1>Sign-in</h1>
         <form>
             <h5>E-mail</h5>
-            <input type="email" value='email' />
+            <input type="text" value={email} onChange={e => setEmail(e.target.value)} />
+
+            <h5>Password</h5>
+            <input type="Password" value={password} onChange={e => setPassword(e.target.value)} />
+            <button type='submit' onClick={signIn} className='login__signInButton'>Sing In</button>
         </form>
+        <p>
+          By signing-in you agree to the AMAZON FAKE CLONE Conditions of Use & Sale. Please
+          see our Privacy Notice, our Cookies Notice and our Interest-Based Ads Notice.
+      </p>
+
+      <button onClick={register} className='login__registerButton'>Create your Amazon Account</button>
+
+        
       </div>
     </div>
   )
