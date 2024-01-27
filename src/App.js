93f15d532e3dcaaebfc15098e87f4f7d7a384c32
Getnet Alemye <debreroha12@gmail.com>
2024-01-27 14:05:46 +0300

    firebase d3

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import Header from './Header'
 import Home from './Home'
 import Checkout from './Checkout'
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Login from './Login'
 
 
@@ -10,15 +10,24 @@ function App() {
   return (
     <Router>
       <div className="app">
-        <Routes>
-          <Route path="/login" element={ <Login key="login" />} />
-          <Route path="/" element={[<Header key="header" />, <Home key="home" />]} />
-          <Route path="/checkout" element={[<Header key="header" />, <Checkout key="Checkout" />]} />
+        <Switch>
+        <Route path="/login">
+            <Login />
+          </Route>          
+          <Route path="/">
+            <Header />
+            <Home />
+          </Route>          
+          <Route path="/checkout">
+            <Header />
+            <Checkout />
+          </Route>          
           {/* <Route path='/checkout' element={<Checkout />} /> */}
-          </Routes>
+          </Switch>
       </div>
     </Router>
   );
 }
 
 export default App;
+
